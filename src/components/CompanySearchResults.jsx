dfa9d202ee0501c65480e223c928366c674b5b65
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-11 16:55:07 +0200

    eslint

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -12,6 +12,7 @@ const CompanySearchResults = () => {
 
   useEffect(() => {
     dispatch(getCompanyAction(params));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
